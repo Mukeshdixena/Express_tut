2d0273f47c680b8eb092f76f5ca9a0c40b5ade59
Mukesh <mukeshdixena123@gmail.com>
2025-01-08 22:11:32 +0530

    Adding 404 Error page

---
+++ tut_06 - Adding 404 Error Page/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const adminRoutes = require('./Routes/admin')
+const shopRoutes = require('./Routes/shop')
+
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1> Page not found</h1>');
+})
+
+app.listen(3000)
