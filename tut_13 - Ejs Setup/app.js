f0d5ff85f1bb4448cfc34def5aef1900ca4d4ecd
Mukesh <mukeshdixena123@gmail.com>
2025-01-19 22:25:30 +0530

    Ejs Setup

--- tut_13 - Ejs Setup/app.js
+++ tut_13 - Ejs Setup/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', path.join(__dirname, 'views'));
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
